6c2325413e8e1e725f99cd69bc51175952b240c9
Christopher You <christopheryou@ufl.edu>
2021-03-05 16:40:53 -0500

    Added notifications and alarm, will need tweaking.

--- BaseChromeExtension/background.js
+++ BaseChromeExtension/background.js
@@ -12,3 +12,33 @@ chrome.runtime.onInstalled.addListener(function() {
       }]);
     });
   });
+
+// Currently alarm is set to go off every 25 minutes
+// We will need to update this var (25)
+// Uncomment when you want the alarm to start.
+// var alarm = chrome.alarms.create("Pomodoro Timer", {periodInMinutes: 25});
+
+chrome.alarms.onAlarm.addListener( function (alarm) {
+  createNotification();
+  audioNotification();
+});
+  
+function audioNotification(){
+    var yourSound = new Audio('audio/mario_coin.mp3');
+    yourSound.play();
+}
+  
+function createNotification(){
+  var opt;
+  // if (state == nudge_1):
+  opt = {
+      type: "basic",
+      title: "Time to take a break!",
+      message: "Did you know if you snooze your breaks you will literally die?",
+      iconUrl: "images/teapot.png",
+    };
+    chrome.notifications.create("BreakTimeNotification", opt, function(){});
+  
+    //include this line if you want to clear the notification after 5 seconds
+    // setTimeout(function(){chrome.notifications.clear("notificationName",function(){});},5000);
+}
